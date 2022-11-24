49b2f21ce246015d21e052b6a30749e4cf04c766
Rishi kumar <rishi@Rishis-Air.mshome.net>
2022-11-24 12:10:11 +0530

    This is version1 of chatapp okie

---
+++ chatapp_test1/lib/pages/search_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController searchController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Search",
+          style: TextStyle(
+              fontSize: 27, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Theme.of(context).primaryColor,
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: searchController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search Groups",
+                        hintStyle:
+                            TextStyle(color: Colors.white, fontSize: 16)),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ chatapp_test1/lib/service/database_service.dart
@@ -0,0 +1,99 @@
+import 'package:chatapp_test1/pages/group_info.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  final String? uid;
+  DatabaseService({this.uid});
+
+  //reference for our collections
+  final CollectionReference userCollection =
+      FirebaseFirestore.instance.collection("users");
+
+  final CollectionReference groupCollection =
+      FirebaseFirestore.instance.collection("groups");
+
+  //saving the userdata
+  Future savingUserData(String fullName, String email) async {
+    return await userCollection.doc(uid).set({
+      "fullName": fullName,
+      "enmail": email,
+      "groups": [],
+      "profilePic": "",
+      "uid": uid,
+    });
+  }
+
+  //getting user data
+  Future gettingUserData(String email) async {
+    QuerySnapshot snapshot =
+        await userCollection.where("email", isEqualTo: email).get();
+    return snapshot;
+  }
+
+  //get user group
+  getUserGroups() async {
+    return userCollection.doc(uid).snapshots();
+  }
+
+  //creating a group
+  Future createGroup(String userName, String id, String groupName) async {
+    DocumentReference groupDocumentReference = await groupCollection.add({
+      "groupName": groupName,
+      "groupIcon": "",
+      "admin": "${id}_$userName",
+      "members": [],
+      "recentMessage": "",
+      "recentMessageSender": "",
+    });
+    //update the member
+    await groupDocumentReference.update({
+      "members": FieldValue.arrayUnion(["${uid}_$userName"]),
+      "groupId": groupDocumentReference.id,
+    });
+
+    DocumentReference userDocumentReference = userCollection.doc(uid);
+    return await userDocumentReference.update({
+      "groups":
+          FieldValue.arrayUnion(["${groupDocumentReference.id}_$groupName"])
+    });
+  }
+
+  //getting the chats
+  getChats(String groupId) async {
+    return groupCollection
+        .doc(groupId)
+        .collection("messages")
+        .orderBy("time")
+        .snapshots();
+  }
+
+  Future getGroupAdmin(String groupId) async {
+    DocumentReference d = groupCollection.doc(groupId);
+    DocumentSnapshot documentSnapshot = await d.get();
+    return documentSnapshot['admin'];
+  }
+
+  //get group members
+  getGroupMembers(groupId) async {
+    return groupCollection.doc(groupId).snapshots();
+  }
+
+  //search
+  searchByName(String groupName) {
+    return groupCollection.where("groupName.", isEqualTo: groupName).get();
+  }
+
+  //function --> bool, take user value and check whether that user is available in the group
+  Future<bool> isUserJoined(
+      String groupName, String groupId, String userName) async {
+    DocumentReference userDocumentReference = userCollection.doc(uid);
+    DocumentSnapshot documentSnapshot = await userDocumentReference.get();
+
+    List<dynamic> groups = await documentSnapshot['groups'];
+    if (groups.contains("${groupId}_$groupName")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
